d0067ab8cffe03b34b3c155e9fd80aff2279f6a5
Alwaleed Ahmed <alwleedamado@gmail.com>
2024-12-25 22:48:52 +0200

    Add project files.

---
+++ OrbitHomeCinema/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OrbitHomeCinema.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ OrbitHomeCinema/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OrbitHomeCinema.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
